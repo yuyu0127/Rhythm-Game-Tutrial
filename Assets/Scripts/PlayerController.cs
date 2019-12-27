4bfbac06a119b63fc9a3f2332f68f61b3176c607
yuyu0127 <saiyuki01271210@yahoo.co.jp>
2019-12-27 21:24:26 +0900

    1.6.8「シングルノーツを生成しよう」完了

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+	[SerializeField] private GameObject prefabSingleNote; // 生成するPrefab
+
 	public static float ScrollSpeed = 1.0f; // 譜面のスクロール速度
 	public static float CurrentSec = 0f; // 現在の経過時間(秒)
 	public static float CurrentBeat = 0f; // 現在の経過時間(beat)
@@ -16,8 +19,42 @@ public class PlayerController : MonoBehaviour
 		CurrentBeat = 0f;
 
 		// TODO: ここで譜面の読み込みを行う
+		// 現段階では手打ち
 
-		// TODO: ここでノーツの生成を行う
+		// Beatmapクラスのインスタンスを作成
+		beatmap = new Beatmap();
+
+		// ノーツ配置情報を設定
+		beatmap.noteProperties = new List<NoteProperty>
+		{
+			new NoteProperty(0, 0, 0, NoteType.Single),
+			new NoteProperty(1, 1, 1, NoteType.Single),
+			new NoteProperty(2, 2, 2, NoteType.Single),
+			new NoteProperty(3, 3, 1, NoteType.Single),
+			new NoteProperty(4, 4, 0, NoteType.Single),
+			new NoteProperty(4, 4, 4, NoteType.Single),
+			new NoteProperty(5, 5, 3, NoteType.Single),
+			new NoteProperty(6, 6, 2, NoteType.Single),
+			new NoteProperty(7, 7, 3, NoteType.Single),
+			new NoteProperty(8, 8, 4, NoteType.Single)
+		};
+
+		// テンポ変化を設定
+		beatmap.tempoChanges = new List<TempoChange>
+		{
+			new TempoChange(0, 60f),
+			new TempoChange(2, 120f),
+			new TempoChange(4, 60f),
+			new TempoChange(6, 120f)
+		};
+
+		// ノーツの生成を行う
+		foreach (var noteProperty in beatmap.noteProperties)
+		{
+			// beatmapのnotePropertiesの各要素の情報からGameObjectを生成
+			var objNote = Instantiate(prefabSingleNote);
+			objNote.GetComponent<NoteControllerBase>().noteProperty = noteProperty;
+		}
 	}
 
 	void Update()
